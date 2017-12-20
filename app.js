547c0c0a954040d9e7f891a045d44848299c0ea4
Ulisses Caon <ulissescaon@gmail.com>
2017-12-20 15:11:31 -0200

    Standard JS

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-const axios  = require('axios')
+const axios = require('axios')
 const config = require('./config/master')
-const http   = require('http')
+const http = require('http')
 
 const sphynx = async (req, res, next) => {
-    try {
-        const token = req.headers['x-auth']
-        const user  = await axios({
-            method  : 'get',
-            url     : config.target.uri,
-            headers : {
-                "x-auth": token
-            }
-        })
+  try {
+    const token = req.headers['x-auth']
+    const user = await axios({
+      method: 'get',
+      url: config.target.uri,
+      headers: {
+        'x-auth': token
+      }
+    })
 
-        req.user = user
-        next()
-
-    } catch(e) {
-        res.status(500).send({message: 'An error occured', e})
-    }
+    req.user = user
+    next()
+  } catch (e) {
+    res.status(500).send({message: 'An error occured', e})
+  }
 }
 
-module.exports = {sphynx}
+module.exports = { sphynx }
--- config/master.js
+++ config/master.js
@@ -2,7 +2,7 @@
  * You may define your custom route in here
  */
 module.exports = {
-    target: {
-        uri: 'http://localhost:3000/auth/'
-    }
+  target: {
+    uri: 'http://localhost:3000/auth/'
+  }
 }
